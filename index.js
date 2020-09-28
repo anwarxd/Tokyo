ede81f194e999db77fd54d0d0f0e98ee3155ec09
anwarxd <71465683+anwarxd@users.noreply.github.com>
2020-09-28 18:08:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require("fs");
 
-bot.on("ready", function() {
-    console.log('Ready');
+// Global Settings
+const prefix = '</';
+
+bot.on('message', async message => {
+
+    let msg = message.content.toUpperCase();
+    let sender = message.author;    
+    let args = message.conten.slice(prefix.length).trim().split(/ +/g);
+    let command = args.shift().toLowerCase();
+
+    if (msg ===prefix + 'PING') {
+        message.channel.send('PONG!')
+    }
+
+
+    var dogs = ["674516418641", "76527952860925752", "7628960184827756", "7825627628572680982", "758728962852y772", "7578262987628098"]
+    var throwDog = dogs[Math.floor(Math.random() * dogs.length)];
+
+    if (msg === prefix + 'DOG') {
+        message.channel.send(`Ini Waifumu Mas! \n${throwDog}`);
+    }
+
+
 });
 
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (message.content == "Hello") {
-        message.channel.sendMessage("Hai Juga");
-    }
+bot.on("ready", () => {
+    console.log('BOT STARTED.');
 });
 
 bot.login(process.env.BOT_TOKEN);
